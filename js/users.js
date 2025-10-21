89c01b0d6fce846c452a155b3f072cdb1d104dbc
ivo23975-art <ivo23975@gmail.com>
2025-10-21 14:42:02 +0200

    Update users.js
    h

--- js/users.js
+++ js/users.js
@@ -1,1 +1,10 @@
+// ==============================
+// 🔐 ARCHIVIO UTENTI SIMULATO
+// ==============================
+
+const users = [
+  { username: "chiara", password: "condo2025", role: "condomino" },
+  { username: "admincondominio", password: "gestione2025", role: "admin-condominio" },
+  { username: "adminsito", password: "sito2025", role: "admin-sito" }
+];
 
